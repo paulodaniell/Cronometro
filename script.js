e50cd2a221a7fa4af1d20867597f2858e1d7f56b
Paulo Daniel <paulodanielm19@gmail.com>
2026-02-08 21:47:40 -0300

    docs: adiciona README e arquivos iniciais do projeto

---
+++ script.js
@@ -0,0 +1,42 @@
+function relogio(){
+    function getTime(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR',{
+        hour12:false,
+        timeZone: 'UTC'
+    });
+}
+const relogio = document.querySelector('.relogio')
+let segundos=0;
+let timer;
+
+function iniciaRelogio(){
+    timer = setInterval(function(){
+        segundos++
+        relogio.innerHTML =getTime(segundos);
+    },1000);
+    
+}
+document.addEventListener('click',function(e){
+    const elemento = e.target;
+
+    if(elemento.classList.contains('zerar')){
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        relogio.classList.remove('pausado');
+        segundos=0;
+    }
+    if(elemento.classList.contains('pausar')){
+        clearInterval(timer);
+        relogio.classList.add('pausado');
+    }
+    if(elemento.classList.contains('iniciar')){
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+});
+
+}
+relogio();
+
